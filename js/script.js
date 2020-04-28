dbd84b32b5adcfaff4dd481b70829ed83eaed8be
Oksana.Zembal <oksanazembal@gmail.com>
2020-04-28 11:22:35 +0300

    added introduce block

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 $('.intro__slider').slick({
     infinite: true,
-    dots: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    dots: false,
+    responsive: [{
+            breakpoint: 768,
+            settings: {
+
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+
+                slidesToShow: 1
+            }
+        }
+    ],
+    prevArrow: "<img class='a-left control-c prev slick-prev' src='img/icon/left-arrow.png'>",
+    nextArrow: "<img class='a-right control-c next slick-next' src='img/icon/right-arrow.png'>"
 });
