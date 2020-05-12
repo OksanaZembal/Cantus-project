dd85c2c5b4d3731b08212faa9d179bcbe893a77e
Oksana.Zembal <oksanazembal@gmail.com>
2020-05-12 22:33:52 +0300

    added next block

--- js/script.js
+++ js/script.js
@@ -21,3 +21,36 @@ $('.intro__slider').slick({
     prevArrow: "<img class='a-left control-c prev slick-prev' src='img/icon/left-arrow.png'>",
     nextArrow: "<img class='a-right control-c next slick-next' src='img/icon/right-arrow.png'>"
 });
+
+//$('.upcoming__slider-slides').slick({
+//   infinite: true,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   dots: false,
+//});
+
+$('.latest__slider__cart').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    responsive: [{
+            breakpoint: 1200,
+            settings: {
+
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+
+                slidesToShow: 1
+            }
+        }
+
+    ],
+    prevArrow: "<img class='a-left control-c prev slick-prev' src='img/icon/left-arrow.png'>",
+    nextArrow: "<img class='a-right control-c next slick-next' src='img/icon/right-arrow.png'>"
+});
